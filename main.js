1253ff82e882fcc6022abd89a85912e5ee2be1b1
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-10 14:01:14 +0300

    Combining the number change and counter

--- main.js
+++ main.js
@@ -9,24 +9,39 @@ num.textContent = count
 dec.addEventListener("click", event => {
     let numb = --count
     num.textContent = numb
+    if (numb > 1) {
+        num.style.color = "red";
+    }
+    if (numb < 0) {
+        num.style.color = "blue";
+    }
+    if (numb = 0) {
+        num.style.color = "black";
+    }
 })
 inc.addEventListener("click", event => {
     let numb = ++count
     num.textContent = numb
+    if (numb > 1) {
+        num.style.color = "red";
+    }
+    if (numb < 0) {
+        num.style.color = "blue";
+    }
+    if (numb = 0) {
+        num.style.color = "black";
+    }
 })
 reset.addEventListener("click", event => {
     let numb = 0
     num.textContent = numb
+    if (numb > 1) {
+        num.style.color = "red";
+    }
+    if (numb < 0) {
+        num.style.color = "blue";
+    }
+    if (numb = 0) {
+        num.style.color = "black";
+    }
 })
-
-alert(count)
-
-if (count > 1) {
-    num.style.color = "red";
-}
-if (numb < 0) {
-    num.style.color = "blue";
-}
-if (numb = 0) {
-    num.style.color = "black";
-}
