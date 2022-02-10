77ac49aa0e7afa940926192db614afc7869fd81d
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-10 12:28:14 +0300

    adding color changes with number

---
+++ main.js
@@ -0,0 +1,15 @@
+let dec = document.getElementById("dec")
+let inc = document.getElementById("inc")
+let reset = document.getElementById("reset")
+let num = document.getElementById("num")
+
+
+num.textContent = 1
+
+numb = parseInt(num)
+
+if (numb = 0) {
+    num.style.color = "red"
+} else {
+    num.style.color = "blue"
+}
